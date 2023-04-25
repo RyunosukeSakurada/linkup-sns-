8e7f38e075efb1f0901393d79ee2ad629f5f5d64
RyunosukeSakurada <longzhizhu513@gmail.com>
2023-04-25 05:22:00 -0700

    added some components

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Topbar from '../../components/topbar/Topbar'
+import Sidebar from "../../components/sidebar/Sidebar"
+import Timeline from "../../components/timeline/Timeline"
+import Ads from "../../components/ads/Ads"
 
 function Home() {
   return (
     <>
       <Topbar />
+      <Sidebar />
+      <Timeline />
+      <Ads />
     </>
   )
 }
