35e46448bb12f3338013c69cf2332e1175b48aea
RyunosukeSakurada <longzhizhu513@gmail.com>
2023-04-30 00:45:39 -0700

    fixed register API

--- frontend/src/state/AuthContext.js
+++ frontend/src/state/AuthContext.js
@@ -4,17 +4,6 @@ import AuthReducer from "./AuthReducer";
 //Define the first user
 const initialState = {
   user: JSON.parse(localStorage.getItem("user")) || null,
-  // user:{
-  //   _id : "6446457e3cadf75adbfe483b",
-  //   fullname: "Ryunosuke Sakurada",
-  //   username:"ronskrd",
-  //   email: "longzhizhu513@gmail.com",
-  //   password:"Longzhizhu513",
-  //   profilePicture: "/person/1.jpg",
-  //   followers: [],
-  //   followings: [],
-  //   isAdmin: false
-  // },
   isFetching:false,
   error:false
 }
